4c9f221482e457066c7b6fdded7a5bdbe0b61054
Bobby Cahill <bobby@bobbycahill.com>
2021-12-12 15:28:32 -0500

    Day 2, Part 1 skeleton

---
+++ src/2-dive/divePart1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+    // Open file and read in one line at a time
+
+        // Figure out how to read in string[space]int from a line
+
+        // Initialize global values and move other logic to a func
+
+    // Output final total
+}
